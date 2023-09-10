8a2baf4dec16b6d09c7e02948b0eff8d1e59ddf5
YulianSan <knoob0733@gmail.com>
2023-09-10 17:40:17 -0300

    feat: adding hook to handle scroll of slide

---
+++ src/hooks/useScrollSide.js
@@ -0,0 +1,14 @@
+import { useRef } from "react"
+
+export function useScrollSlide(x = 0, y = 0) {
+    const slide = useRef()
+    const nextProduct = () => {
+        slide.current.scrollBy(x, y)
+    }
+
+    const prevProduct = () => {
+        slide.current.scrollBy(-x, y)
+    }
+
+    return { slide, prevProduct, nextProduct }
+}
