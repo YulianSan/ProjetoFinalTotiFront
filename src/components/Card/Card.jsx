c6972d1c40ead0de1384d6fedd04da178f2b87f6
YulianSan <knoob0733@gmail.com>
2023-09-16 18:19:33 -0300

    fix: removing max-width

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 export function Card({ children }) {
     return (
-        <article className="rounded-xl shadow-xl shadow-zinc-300 max-w-[14rem] w-full px-4 pb-4 bg-white flex-none snap-start">
+        <article className="rounded-xl shadow-xl shadow-zinc-300 w-full px-4 pb-4 bg-white flex-none snap-start">
             { children }
         </article>
     )
