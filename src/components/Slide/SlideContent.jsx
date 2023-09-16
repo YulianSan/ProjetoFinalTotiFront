4fdc5f4e9f15fff8f176820a482829dca923ec92
YulianSan <knoob0733@gmail.com>
2023-09-16 18:20:28 -0300

    feat: slide jump all products visible

--- src/components/Slide/SlideContent.jsx
+++ src/components/Slide/SlideContent.jsx
@@ -2,7 +2,12 @@ import React from "react"
 
 export const SlideContent = React.forwardRef(({ children }, ref) => {
     return (
-        <div ref={ref} className="flex overflow-x-scroll gap-x-4 scroll-smooth snap-x no-scrollbar">
+        <div 
+            ref={ref} 
+            className="flex overflow-x-scroll gap-x-5 scroll-smooth snap-x 
+                snap-mandatory no-scrollbar xl:[&>*]:w-[calc(20%-1rem)] 
+                lg:[&>*]:w-[calc(25%-1rem)] md:[&>*]:w-[calc(33.34%-1rem)] 
+                sm:[&>*]:w-[calc(50%-1rem)] py-8">
             { children }
         </div>
     )
