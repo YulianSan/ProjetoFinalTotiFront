8e2b02f65457cda353a8002aba19fb637aecdc6c
YulianSan <knoob0733@gmail.com>
2023-09-16 18:18:46 -0300

    feat: get x and y by function

--- src/hooks/useScrollSide.js
+++ src/hooks/useScrollSide.js
@@ -1,13 +1,13 @@
 import { useRef } from "react"
 
-export function useScrollSlide(x = 0, y = 0) {
+export function useScrollSlide(x = () => 0, y = () => 0) {
     const slide = useRef()
     const nextProduct = () => {
-        slide.current.scrollBy(x, y)
+        slide.current.scrollBy(x(), y)
     }
 
     const prevProduct = () => {
-        slide.current.scrollBy(-x, y)
+        slide.current.scrollBy(-x(), y)
     }
 
     return { slide, prevProduct, nextProduct }
