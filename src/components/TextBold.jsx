1d4a6e473d94dee28e3d5815333d49bbf37e19ae
YulianSan <knoob0733@gmail.com>
2023-09-16 21:21:20 -0300

    fix: change style

--- src/components/TextBold.jsx
+++ src/components/TextBold.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export function TextBold({ children }) {
     children = React.cloneElement(children, { 
-        className: `font-bold text-zinc-800 ${ children?.props?.className ?? '' }`}
+        className: `font-bold text-zinc-600 ${ children?.props?.className ?? '' }`}
     )
     return (
         <>
