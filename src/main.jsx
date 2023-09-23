8f21f4430c2b2f8137339b17bf92150a635414cb
YulianSan <knoob0733@gmail.com>
2023-09-23 18:20:09 -0300

    feat: adding pages in router

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Home } from './pages/Home.jsx'
+import { Login } from './pages/Login.jsx'
+import { Singup } from './pages/Singup.jsx'
 import { Product } from './pages/Product.jsx'
 
 const router = createBrowserRouter([
@@ -16,8 +18,16 @@ const router = createBrowserRouter([
                 element: <Home title="Home"/>
             },
             {
+                path: '/login',
+                element: <Login title="Login"/>
+            },
+            {
+                path: '/singup',
+                element: <Singup title="Singup"/>
+            },
+            {
                 path: '/product/:id',
-                element: <Product />
+                element: <Product title="Detalhes do produto"/>
             }
         ]
     }
