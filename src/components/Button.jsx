9f5e840f2e3ded2eaa290d8ddd966bb60b027cca
YulianSan <knoob0733@gmail.com>
2023-09-23 18:16:05 -0300

    feat: adding type in component button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,15 @@
-export function Button({ click, children }){
+export function Button({ click, children, type }){
     return(
         <button 
             onClick={click} 
+            type={type}
             className='bg-red-500 text-white px-6 font-bold rounded-md py-2 
                 w-full text-center flex justify-center my-4'>
             {children}
         </button>
     )
+}
+
+Button.defaultProps = {
+    type: 'button'
 }
