f6a3c359c069c6cb9c494fa6b5f8a8d2749336ca
YulianSan <knoob0733@gmail.com>
2023-10-01 16:02:37 -0300

    feat: change style

--- src/components/Card/CardStoreProduct.jsx
+++ src/components/Card/CardStoreProduct.jsx
@@ -3,7 +3,7 @@ import { ButtonEdit } from '../Button/ButtonEdit'
 
 export function CardStoreProduct({ data, column }) {
     return (
-        <div className="flex flex-wrap flex-col md:flex-row">
+        <div className="flex flex-wrap flex-col gap-x-2 md:flex-row">
             <div className="flex-1 md:block gap-4 flex">
                 <div className="font-lightlight text-zinc-400 uppercase">
                     Name
@@ -28,7 +28,7 @@ export function CardStoreProduct({ data, column }) {
                     {data['price']}
                 </div>
             </div>
-            <div className="flex gap-1 flex-none align-middle">
+            <div className="flex-1 flex gap-1 items-center justify-center">
                 <ButtonEdit />
                 <ButtonDelete />
             </div>
