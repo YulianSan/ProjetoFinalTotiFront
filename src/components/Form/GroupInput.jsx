b35ac6e41e4f2dbfb2718ff71b2c126276596733
YulianSan <knoob0733@gmail.com>
2023-10-01 15:04:01 -0300

    fix: indent code

--- src/components/Form/GroupInput.jsx
+++ src/components/Form/GroupInput.jsx
@@ -2,13 +2,13 @@ import React from "react"
 
 export function GroupInput({ children, id }) {
 
-    children = children.map(( element, key ) => {
+    children = children.map((element, key) => {
         return React.cloneElement(element, { id, key })
     })
 
     return (
         <div className="flex flex-col">
-            { children }
+            {children}
         </div>
     )
 }
