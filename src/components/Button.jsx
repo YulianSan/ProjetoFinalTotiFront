5d59e50d33cd7ceb08dd4d1ef4ac55d6248d5cec
YulianSan <knoob0733@gmail.com>
2023-10-04 17:53:26 -0300

    feat: allow custom props and adding style when is disabled

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
-export function Button({ click, children, type }){
-    return(
-        <button 
-            onClick={click} 
+export function Button({ click, children, type, ...rest }) {
+    return (
+        <button
+            onClick={click}
             type={type}
+            {...rest}
             className='bg-red-500 text-white px-6 font-bold rounded-md py-2 
-                w-full text-center flex justify-center my-4'>
+                w-full text-center flex justify-center my-4 disabled:bg-red-400'>
             {children}
         </button>
     )
